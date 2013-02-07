a31f1897a3897b6a2f06ae04b28479255b4d5b97
ScottishDave <dmckeown@bgtpartners.com>
2013-02-07 10:31:30 -0500

    First init

---
+++ stagegit.php
@@ -0,0 +1,10 @@
+<?php
+
+var_dump($_POST['payload']);
+
+$my_file = 'payload.json';
+$handle = fopen($my_file, 'w') or die('Cannot open file:  '.$my_file);
+$data = $_POST['payload'];
+fwrite($handle, $data);
+
+?>
